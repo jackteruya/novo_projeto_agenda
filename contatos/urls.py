fa0ff7ac3c009237875807495a4496ab6c32c0cd
jackteruya <jack.teruya@gmail.com>
2021-09-01 06:56:40 -0400

    Adicionado filtros e campo de busca no contatos

--- contatos/urls.py
+++ contatos/urls.py
@@ -4,5 +4,7 @@ from contatos import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('<int:id>/', views.ver_contato, name='ver_contato'),
+
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from contatos.models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 5)
 
     page = request.GET.get('page')
@@ -25,6 +29,32 @@ def ver_contato(request, id):
     #     raise Http404()
 
     contato = get_object_or_404(Contato, id=id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    if termo is None:
+        raise Http404()
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('page')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
     })
