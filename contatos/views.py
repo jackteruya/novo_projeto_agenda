7a0ee4da8b44b1623fcca7029d935d6a9c804278
jackteruya <jack.teruya@gmail.com>
2021-08-31 23:48:58 -0400

    Adicionado no model contatos o campo mostrar; Adicionado a paginação no template.

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from contatos.models import Contato
+from django.core.paginator import Paginator
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 5)
+
+    page = request.GET.get('page')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
